af6aa3fcd266d4c0f1132d8a4bda897abed0a942
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-05-29 01:02:30 -0500

    Update position_control.m

--- Simulation/position_control.m
+++ Simulation/position_control.m
@@ -20,4 +20,42 @@ phi = states(10);
 theta = states(11);
 psi = states(12);
 
+%% 1. Calculating xdot, ydot and zdot through R*[u;v;w]
+% Rotation matrix z-y-x
+Rzyx = [cos(theta)*cos(psi) sin(phi)*sin(theta)*cos(phi)-cos(phi)*sin(psi)  cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi); ...
+        cos(theta)*sin(psi) sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi)  cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi); ...
+        -sin(theta)         sin(phi)*cos(theta)                             cos(phi)*cos(theta)];
+
+xdot = [1 0 0]*Rzyx*[u;v;w];
+ydot = [0 1 0]*Rzyx*[u;v;w];
+zdot = [0 0 1]*Rzyx*[u;v;w];
+
+
+%% 2. Computing the ux, uy and uz
+%%% k1= -2 and k2 = -3
+% [x_ref,x_dot_ref,y_ref,y_dot_ref,z_ref,z_dot_ref,psi_ref]
+k1 = -2; k2 = -3;
+ex = x_ref - x;
+ey = y_ref - y ;
+ez = z_ref - z;
+exdot = x_dot_ref - xdot;
+eydot = y_dot_ref - ydot;
+ezdot = z_dot_ref - zdot;
+
+ux = k1*ex + k2*exdot;  % edotdotx
+uy = k1*ey + k2*eydot;  % edotdoty
+uz = k1*ez + k2*ezdot;  % edotdotz
+
+% Calculatin xdd=vx, ydd=vy and zdd=vz
+vx = x_dotdot_ref - ux; % vx = xdd
+vy = y_dotdot_ref - uy; % 
+vz = z_dotdot_ref - uz; % 
+
+a = vx./(vz + g);
+b = vy./(vz + g);
+c = cos(psi_ref);
+d = sin(psi_ref);
+
+
+
 end
