e883afb8bbb2f3ea02f8ee333e1c98be78b834c7
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-06-11 01:42:17 -0500

    Create myDrone.m

---
+++ UAV model/myDrone.m
@@ -0,0 +1,88 @@
+function [UAV] = myDrone()
+  
+% Plot_UAVe(x1(1),y1(1),z1(1),0,0,pi,1.2)
+az = pi;
+scale = 1
+scale=0.3*scale;   
+dx=0;dy=0;dz=0; 
+ax=0;
+ay=0;
+az=0;
+
+%M1=Plot_UAVe(x1(1),y1(1),z1(1),0,0,phi(1),1.2); % Plot UAV initial position x1,y1 and phi orientation
+
+Rx=[ 1, 0, 0; 0, cos(ax), -sin(ax); 0, sin(ax), cos(ax)];
+Ry=[ cos(ay), 0, sin(ay); 0, 1, 0; -sin(ay), 0, cos(ay)];
+Rz=[ cos(az), -sin(az), 0; sin(az), cos(az), 0; 0, 0,1];
+
+R=Rx*Ry*Rz;
+body=[
+-8.524 -3.178 0;
+-8.524 -1.575 0;
+-23.622 -1.575 0;
+-23.622 1.575 0;
+-8.524 1.575 0;
+-8.524 3.178 0;
+-6.884 5.868 0;
+-1.575 8.933 0;
+-1.575 23.622 0;
+1.575 23.622 0;
+1.575 8.933 0;
+6.884 5.868 0;
+8.524 3.178 0;
+8.524 1.575 0;
+23.622 1.575 0;
+23.622 -1.575 0;
+8.524 -1.575 0;
+8.524 -3.178 0;
+7.014 -5.793 0;
+1.575 -8.933 0;
+1.575 -23.622 0;
+-1.575 -23.622 0;
+-1.575 -8.933 0;
+-7.014 -5.793 0;]*1/(3.937*3);
+zb = 0.2;
+
+body1=[body(:,1) body(:,2) body(:,3)+zb];
+bodysize=1;
+
+for k=1:length(body(:,1))-1
+    sp1=[body(k,1) body(k,2) body(k,3);body(k+1,1) body(k+1,2) body(k,3); body(k+1,1) body(k+1,2) body1(k,3); body(k,1)  body(k,2) body1(k,3)]*scale*R;
+  UAV(k)=patch(sp1(:,1)+dx,sp1(:,2)+dy,sp1(:,3)+dz,[0.1 0.1 0.1]);
+if k==length(body(:,1))-1
+    sp1=[body(length(body),1) body(length(body),2) body(k,3); body(1,1) body(1,2) body(k,3); body(1,1) body(1,2) body1(k,3);body(length(body),1) body(length(body),2) body1(k,3)]*scale*R;
+   UAV(k+1)= patch(sp1(:,1)+dx,sp1(:,2)+dy,sp1(:,3)+dz,[0.1 0.1 0.1]);
+end
+end
+
+bodysize=bodysize+k;
+body=body*scale*R;
+body1=body1*scale*R;
+UAV(bodysize+1)=patch(body(:,1)+dx,body(:,2)+dy,body(:,3)+dz,[0 0.6 0.8]);
+UAV(bodysize+2)=patch(body1(:,1)+dx,body1(:,2)+dy,body1(:,3)+dz,[0 0.6 0.8]);
+
+% cubre elices
+a=pi/10;
+th=pi/4:a:2*pi+pi/4;
+
+x=1.4142*cos(th)*0.5;
+y=1.4142*sin(th)*0.5;
+z=0.21*ones(1,length(th));
+
+hp1=[x'-1.7 y' z']*scale*R;    % arriba
+hp2=[x'+1.7 y' z']*scale*R;    % arriba
+
+hp3=[x'-1.7 y' z']*scale*R;
+hp4=[x'+1.7 y' z']*scale*R;
+
+hp5=[x' y'-2 z']*scale*R;
+hp6=[x' y'+2 z']*scale*R;
+
+UAV(bodysize+3)=patch(hp1(:,1)+dx,hp1(:,2)+dy,hp1(:,3)+dz,[0.1 0.1 0.1],'FaceAlpha',0.25);
+UAV(bodysize+4)=patch(hp2(:,1)+dx,hp2(:,2)+dy,hp2(:,3)+dz,[0.1 0.1 0.1],'FaceAlpha',0.25);
+UAV(bodysize+5)=patch(hp3(:,1)+dx,hp3(:,2)+dy,hp3(:,3)+dz,[0.1 0.1 0.1],'FaceAlpha',0.25);
+UAV(bodysize+6)=patch(hp4(:,1)+dx,hp4(:,2)+dy,hp4(:,3)+dz,[0.1 0.1 0.1],'FaceAlpha',0.25);
+UAV(bodysize+7)=patch(hp5(:,1)+dx,hp5(:,2)+dy,hp5(:,3)+dz,[0.1 0.1 0.1],'FaceAlpha',0.25);
+UAV(bodysize+8)=patch(hp6(:,1)+dx,hp6(:,2)+dy,hp6(:,3)+dz,[0.1 0.1 0.1],'FaceAlpha',0.25);
+
+
